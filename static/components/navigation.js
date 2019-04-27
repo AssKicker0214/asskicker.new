290baa41c27b8900afc4b1b6820ca7effad0d5ab
AssKickerSurfaceBook <121584565@163.com>
2019-04-27 19:54:57 +0800

    navigation

--- static/components/navigation.js
+++ static/components/navigation.js
@@ -13,20 +13,47 @@ Vue.component('navigation', {
         /* 折叠导航栏？ */
     } ,
     template: `
-<nav :class="['navigation', fold?'nav-fold':'']">
-    <button class="nav-toggle fas fa-bars"></button>
-    <ul>
-        <li v-for="(tab, ti) in tabs" 
-        :class="['tab', ti === activePath[0] ? 'active-tab':'']">
-            <a v-text="tab.name" :href="tab.link"></a>
-        </li>
-    </ul>
+<nav :class="['navigation', fold?'nav-collapse':'nav-expand']">
+    <div class="nav-folder">
+    <button class="nav-toggle nav-toggle-expand fas fa-bars" @click="toggle()"></button>
+    
+    </div>
+    <div class="nav-content">
+        <button class="nav-toggle nav-toggle-collapse fas fa-chevron-circle-left" @click="toggle()"></button>
+        <header>
+            <img class="nav-portrait" src="/static/assets/portrait.jpg">
+            <div>
+                <span><i class="fas fa-map-marker-alt"></i><span>南京,</span> </span>
+                <span><i class="fas fa-code"></i><span>Coder</span> </span>
+            </div>
+        </header>
+        <ul>
+            <li v-for="(tab, ti) in tabs" 
+            :class="['tab', ti === activePath[0] ? 'active-tab':'']">
+                <a v-text="tab.name" :href="tab.link"></a>
+            </li>
+        </ul>
+        <footer>
+            <label>
+                <a class="fab fa-github"
+                    href="http://github.com/AssKicker0214">
+                </a>
+                <span>github</span>
+            </label>
+            <label>
+                <a class="fas fa-envelope" 
+                href="mailto:asskicker0214@outlook.com">
+                </a>
+                <span>email</span>
+            </label>
+        </footer>
+    </div>
 </nav>
     
     `,
     methods: {
         toggle: function () {
-            this.$emit('')
+            this.$emit('toggle-nav')
         }
     }
 });
