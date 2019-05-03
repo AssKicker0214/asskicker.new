e191bd0b443e1d766f71a6c0d1bfc74c450b87f5
AssKickerSurfaceBook <121584565@163.com>
2019-05-03 23:52:02 +0800

    Article reading page's background

--- static/components/gradual-img.js
+++ static/components/gradual-img.js
@@ -13,26 +13,22 @@ Vue.component('gradual-img', {
 
     },
     created: function () {
-        const interval = 2000;
         const img = new Image();
-        img.style.transition = `${2000}ms`;
+        img.style.transition = `3s`;
         img.src = this.src;
         img.onload = () => {//return;
             const container = document.querySelector(`#${this.id}`);
             const h = img.naturalHeight
                 , w = img.naturalWidth;
-            let r = Math.sqrt(h*h+w*w);
+            let r = Math.sqrt(h * h + w * w);
             img.style.filter = `blur(${r}px)`;
             img.style.opacity = `0`;
             container.appendChild(img);
 
-            setTimeout(()=>{
+            setTimeout(() => {
                 img.style.opacity = '1';
-            }, interval);
-
-            setTimeout(()=>{
                 img.style.filter = `blur(0)`;
-            }, interval*2)
+            }, 100);
         }
     }
 });
